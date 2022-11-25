ce99e089fcafe0efa2b37e267d77985e15240de8
BrunoPlatero <brunoplatero.8@gmail.com>
2022-11-25 00:25:21 +0000

    Print an image of any color

--- src/main.c
+++ src/main.c
@@ -6,14 +6,12 @@ int main() {
 	int w = 1920;
 
 	char *bmp = malloc(h * w * 3);
-	for (int index = 0; index < h * w * 3; index++){
-		bmp[index] = 0xff;
+	for (int index = 0; index < h * w * 3; index = index + 3){
+		bmp[index] = 0x00;
+		bmp[index + 1] = 0x00;
+		bmp[index + 2] = 0xff;
 	}
-	
-
-
     write_bmp("pixel.bmp", bmp, h * w * 3 , w);
-
 }
 
 
